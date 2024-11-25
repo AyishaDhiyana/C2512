7f0df27212e75d119ad48829c329c15626086e9a
Ayisha Dhiyana PS <119615584+AyishaDhiyana@users.noreply.github.com>
2024-11-25 14:43:36 +0530

    Update task1.cpp

--- DAY 14/task1.cpp
+++ DAY 14/task1.cpp
@@ -1,5 +1,81 @@
 TASK 1 :
 ------------------------------------------------------------------
+C++ Program Management in Linux
+
+`Objective`:  
+Familiarize yourself with basic Linux commands 
+by creating, compiling, and managing 
+a simple C++ program within a project directory.
+
+---
+
+`Tasks`:
+
+1. Create a directory called `cpp_project` 
+to store your C++ programs.
+
+ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~$ mkdir cpp_project
+	
+2. Navigate into the `cpp_project` directory.
+
+ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~$ cd cpp_project
+	
+3. Create a C++ file named `first_program.cpp` 
+in the directory.
+
+ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~/cpp_project$ touch first_program.cpp
+	
+4. Write a simple C++ program inside `first_program.cpp` 
+that prints "Hello, World!" to the console.
+
+ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~/cpp_project$ nano first_program.cpp
+
+5. Compile the C++ program into an executable.
+
+ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~/cpp_project$ g++ first_program.cpp
+	
+6. Run the compiled executable to check the output.
+
+ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~/cpp_project$ ./a.out
+Hello World
+	
+7. View the contents of `first_program.cpp` 
+using a Linux command.
+
+ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~/cpp_project$ cat first_program.cpp
+#include<iostream>
+#include<cstdlib>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "Hello World" << endl;
+
+	return EXIT_SUCCESS;
+}
+	
+8. Remove the `first_program.cpp` file after testing it.
+
+ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~/cpp_project$ rm first_program.cpp
+ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~/cpp_project$ ls
+
+9. Check if the directory `cpp_project` is empty and 
+then remove it if it is.
+
+ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~/cpp_project$ cd ..
+ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~$ ls
+a.out    cpp          Desktop    Downloads   hello_world.cpp  Pictures  snap       Videos
+cmd.txt  cpp_project  Documents  helloworld  Music            Public    Templates
+ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~$ rmdir cpp_project
+ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~$ ls
+a.out    cpp      Documents  helloworld       Music     project  snap       Videos
+cmd.txt  Desktop  Downloads  hello_world.cpp  Pictures  Public   Templates
+	
+---------------------------------------------------------------------------
+SOLUTION 
+---------------------------------------------------------------------------
+	
 ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~$ mkdir cpp_project
 ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~$ cd cpp_project
 ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~/cpp_project$ touch first_program.cpp
