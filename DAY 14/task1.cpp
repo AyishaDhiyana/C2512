99f0f2536ab1128045c97f2a4a4c464689a6cb4d
Ayisha Dhiyana PS <119615584+AyishaDhiyana@users.noreply.github.com>
2024-11-25 12:24:33 +0530

    Create task1.cpp

---
+++ DAY 14/task1.cpp
@@ -0,0 +1,26 @@
+ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~$ mkdir cpp_project
+ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~$ cd cpp_project
+ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~/cpp_project$ touch first_program.cpp
+ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~/cpp_project$ nano first_program.cpp
+ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~/cpp_project$ g++ first_program.cpp
+ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~/cpp_project$ ./a.out
+Hello World
+ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~/cpp_project$ cat first_program.cpp
+#include<iostream>
+#include<cstdlib>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "Hello World" << endl;
+
+	return EXIT_SUCCESS;
+}
+ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~/cpp_project$ rm first_program.cpp
+ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~/cpp_project$ ls
+a.out
+ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~/cpp_project$ cd ..
+ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~$ ls
+a.out    cpp          Desktop    Downloads   hello_world.cpp  Pictures  snap       Videos
+cmd.txt  cpp_project  Documents  helloworld  Music            Public    Templates
