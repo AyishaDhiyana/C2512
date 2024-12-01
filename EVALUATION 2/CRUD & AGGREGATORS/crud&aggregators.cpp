16154171b7d4847ec41b6829711edbf5fea4f2a1
Ayisha Dhiyana PS <119615584+AyishaDhiyana@users.noreply.github.com>
2024-12-02 01:44:55 +0530

    Update crud&aggregators.cpp

--- EVALUATION 2/CRUD & AGGREGATORS/crud&aggregators.cpp
+++ EVALUATION 2/CRUD & AGGREGATORS/crud&aggregators.cpp
@@ -122,3 +122,202 @@ int main(void)
     
     return EXIT_SUCCESS;
 }
+void printMenu()
+{
+    cout << "\n=== Doses Administered Vaccination Management System ===\n";
+    cout << "1. Create Vaccination\n";
+    cout << "2. Display All Vaccination\n";
+    cout << "3. Edit Vaccination\n";
+    cout << "4. Delete Vaccination\n";
+    cout << "5. Minimum DosageAdministered\n";
+    cout << "6. Maximum DosageAdministered\n";
+    cout << "7. 2nd Minimum DosageAdministered\n";
+    cout << "8. 2nd Maximum DosageAdministered\n";
+    cout << "9. Exit\n";
+}
+
+void VaccinationManager::create()
+{
+    if (numDoses >= MAX_DOSES)
+    {
+        cout << "Error: Maximum Doses limit reached.\n";
+        return;
+    }
+
+    string VaccinationID;
+    int DosesAdministered;
+
+    cout << "Enter Vaccination ID: ";
+    cin >> VaccinationID;
+
+    if (findIndexById(VaccinationID) != -1)
+    {
+        cout << "Error: Vaccination ID already exists. Please use a unique ID.\n";
+        return;
+    }
+
+    cout << "Enter Number of DosesAdministered: ";
+    cin >> DosesAdministered;
+
+    vaccination[numDoses].VaccinationID = VaccinationID;
+    vaccination[numDoses].DosesAdministered = DosesAdministered;
+    numDoses++;
+
+    cout << "Total bookings till date: " << numDoses << endl;
+    cout << "Vaccination created successfully.\n";
+}
+
+void VaccinationManager::display()
+{
+    if (numDoses == 0)
+    {
+        cout << "No vaccination available to display.\n";
+        return;
+    }
+
+    cout << "------------------------------------------------\n";
+    cout << "| Vaccination ID | Doses Administered |\n";
+    cout << "------------------------------------------------\n";
+    for (int i = 0; i < numDoses; i++) {
+        cout << "| " << setw(10) << vaccination[i].VaccinationID << " | "
+             << setw(12) << vaccination[i].DosesAdministered << " |\n";
+    }
+    cout << "------------------------------------------------\n";
+}
+
+int VaccinationManager::findIndexById(string VaccinationID)
+{
+    for (int i = 0; i < numDoses; i++)
+    {
+        if (vaccination[i].VaccinationID == VaccinationID) {
+            return i;
+        }
+    }
+    return -1;
+}
+void VaccinationManager::updateByID()
+{
+    string VaccinationID;
+    cout << "Enter Vaccination ID to edit: ";
+    cin >> VaccinationID;
+
+    int index = findIndexById(VaccinationID);
+    if (index == -1) {
+        cout << "Error: Booking ID not found.\n";
+        return;
+    }
+    cout << "Current Details - DosesAdministered: " << vaccination[index].DosesAdministered << "\n";
+    cout << "Enter New DosesAdministered: ";
+    cin >> vaccination[index].DosesAdministered;
+    cout << "Vaccination updated successfully.\n";
+}
+
+void VaccinationManager::deleteByID()
+{
+    string VaccinationID;
+    cout << "Enter Booking ID to delete: ";
+    cin >> VaccinationID;
+
+    int index = findIndexById(VaccinationID);
+    if (index == -1)
+    {
+        cout << "Error: Vaccination ID not found.\n";
+        return;
+    }
+
+    for (int i = index; i < numDoses - 1; i++)
+    {
+        vaccination[i] = vaccination[i + 1];
+    }
+    numDoses--;
+
+    cout << "Vaccination deleted successfully.\n";
+}
+
+bool Vaccination::GreaterThan(const Vaccination& other)
+{
+    return (DosesAdministered > other.DosesAdministered);
+}
+bool Vaccination::LessThan(const Vaccination& other)
+{
+    return (DosesAdministered < other.DosesAdministered);
+}
+
+string Vaccination::GetVaccinationID()
+{
+    return VaccinationID;
+}
+
+int Vaccination::GetDosesAdministered()
+{
+    return DosesAdministered;
+}
+
+int VaccinationAggregator::findMin(VaccinationManager& manager)
+{
+    Vaccination* arr = manager.vaccination;
+    int& n = manager.numDoses;
+    //
+    int minIndex = 0;
+    for (int i = 1; i < n; i++) {
+        if (arr[i].LessThan(arr[minIndex])) {
+            minIndex = i;
+        }
+    }
+    return minIndex;
+}
+
+int VaccinationAggregator::findMax(VaccinationManager& manager)
+{
+    Vaccination* arr = manager.vaccination;
+    int& n = manager.numDoses;
+    //
+    int maxIndex = 0;
+    for (int i = 1; i < n; i++) {
+        if (arr[i].GreaterThan(arr[maxIndex])) {
+            maxIndex = i;
+        }
+    }
+    return maxIndex;
+}
+
+int VaccinationAggregator::findSecondMin(VaccinationManager& manager)
+{
+    Vaccination* arr = manager.vaccination;
+    int& n = manager.numDoses;
+    //
+    int minIndex = findMin(manager);
+    int secondMinIndex = (minIndex == 0) ? 1 : 0;
+
+    for (int i = 0; i < n; ++i) {
+        if (i != minIndex) {
+            if (arr[i].LessThan(arr[secondMinIndex])) {
+                secondMinIndex = i;
+            }
+        }
+    }
+    return secondMinIndex;
+}
+
+int VaccinationAggregator::findSecondMax(VaccinationManager& manager)
+{
+    Vaccination* arr = manager.vaccination;
+    int& n = manager.numDoses;
+    //
+    int maxIndex = findMin(manager);
+    int secondMaxIndex = (maxIndex == 0) ? 1 : 0;
+
+    for (int i = 0; i < n; ++i) {
+        if (i != maxIndex) {
+            if (arr[i].LessThan(arr[secondMaxIndex])) {
+                secondMaxIndex = i;
+            }
+        }
+    }
+    return secondMaxIndex;
+}
+
+VaccinationManager::VaccinationManager()
+{
+    numDoses = 0;
+}
