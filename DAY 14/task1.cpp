bbe53ac6a6b24492ae8eb56fd39bf3d81a93c646
Ayisha Dhiyana PS <119615584+AyishaDhiyana@users.noreply.github.com>
2024-12-03 15:59:34 +0530

    Update task1.cpp

--- DAY 14/task1.cpp
+++ DAY 14/task1.cpp
@@ -10,39 +10,43 @@ a simple C++ program within a project directory.
 ---
 
 `Tasks`:
-
+--------------------------------------------------------------------------
 1. Create a directory called `cpp_project` 
 to store your C++ programs.
 
-ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~$ mkdir cpp_project
-	
+ mkdir cpp_project
+--------------------------------------------------------------------------	
 2. Navigate into the `cpp_project` directory.
 
-ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~$ cd cpp_project
-	
+ cd cpp_project
+--------------------------------------------------------------------------	
 3. Create a C++ file named `first_program.cpp` 
 in the directory.
 
-ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~/cpp_project$ touch first_program.cpp
-	
+ touch first_program.cpp
+---------------------------------------------------------------------------	
 4. Write a simple C++ program inside `first_program.cpp` 
 that prints "Hello, World!" to the console.
 
-ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~/cpp_project$ nano first_program.cpp
-
+ nano first_program.cpp
+ - Ctrl + X 
+ - Y
+ - Enter
+---------------------------------------------------------------------------
 5. Compile the C++ program into an executable.
 
-ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~/cpp_project$ g++ first_program.cpp
-	
+ g++ first_program.cpp
+---------------------------------------------------------------------------	
 6. Run the compiled executable to check the output.
 
-ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~/cpp_project$ ./a.out
+ ./a.out
 Hello World
-	
+---------------------------------------------------------------------------	
 7. View the contents of `first_program.cpp` 
 using a Linux command.
 
-ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~/cpp_project$ cat first_program.cpp
+cat first_program.cpp
+	
 #include<iostream>
 #include<cstdlib>
 using std::cout;
@@ -54,25 +58,28 @@ int main()
 
 	return EXIT_SUCCESS;
 }
-	
+---------------------------------------------------------------------------	
 8. Remove the `first_program.cpp` file after testing it.
 
-ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~/cpp_project$ rm first_program.cpp
-ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~/cpp_project$ ls
-
+ rm first_program.cpp
+- ls
+---------------------------------------------------------------------------
 9. Check if the directory `cpp_project` is empty and 
 then remove it if it is.
 
-ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~/cpp_project$ cd ..
-ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~$ ls
-a.out    cpp          Desktop    Downloads   hello_world.cpp  Pictures  snap       Videos
-cmd.txt  cpp_project  Documents  helloworld  Music   Public    Templates
-ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~$ rmdir cpp_project
-ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~$ ls
-a.out    cpp      Documents  helloworld       Music     project  snap       Videos
-cmd.txt  Desktop  Downloads  hello_world.cpp  Pictures  Public   Templates
-	
+ cd ..
+- ls
+ rmdir cpp_project
+- ls
+
 ---------------------------------------------------------------------------
+Goals : 
+	By completing these tasks, 
+        you will practice using Linux commands related to file and 
+        directory management, 
+        text editing, 
+        compiling, and 
+        running C++ programs.
 ---------------------------------------------------------------------------
 	
 ayisha-dhiyana-ps@ayisha-dhiyana-ps-VirtualBox:~$ mkdir cpp_project
