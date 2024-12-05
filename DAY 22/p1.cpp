63ec3c233be37f45b7c081f4504631dfb42e0046
Ayisha Dhiyana PS <119615584+AyishaDhiyana@users.noreply.github.com>
2024-12-06 00:26:25 +0530

    Update p1.cpp

--- DAY 22/p1.cpp
+++ DAY 22/p1.cpp
@@ -14,3 +14,24 @@ int main()
         cout << "ref of deref = " << &(*(&id)) << endl;
         return EXIT_SUCCESS;
 }
+--------------------------------------------------------------
+//using pointers
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+        int id = 56;
+        int* ptr = &id;
+        cout << "id = " << id << endl;
+        cout << "address = " << &id << endl;
+        cout << "deref = " << *(&id) << endl;
+        cout << "ref of deref = " << &(*(&id)) << endl;
+        cout << "address of pointer = " << ptr << endl;
+        cout << "ptr value return = " << *ptr << endl;
+        return EXIT_SUCCESS;
+}
+-----------------------------------------------------------------
