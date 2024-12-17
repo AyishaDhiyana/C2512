25949f2d897c43f9709247328562a57c9e572f1f
Ayisha Dhiyana PS <119615584+AyishaDhiyana@users.noreply.github.com>
2024-12-17 10:07:41 +0530

    Update Task 1.1.cpp

--- EVALUATION 4 /Task 1.1.cpp
+++ EVALUATION 4 /Task 1.1.cpp
@@ -1,36 +1,69 @@
 #include <iostream>
-#include <string>
-#include <utility> 
+#include <cstring>
+#include <utility>
+
 using namespace std;
 
 class Employee {
-protected:
+private:
     int id;
     int age;
-    string name;
+    char* name;
 
 public:
-    Employee(int id, int age, const string &name) : id(id), age(age), name(name) {}
-
-    friend ostream &operator<<(ostream &os, const Employee &e) {
-        os << e.id << " " << e.age << " " << e.name;
-        return os;
+    Employee(int id, int age, const char* name) : id(id), age(age) {
+        this->name = new char[strlen(name) + 1];
+        strcpy(this->name, name);
     }
 
-    void swap(Employee &other) {
-        std::swap(id, other.id);
-        std::swap(age, other.age);
-        std::swap(name, other.name);
+    ~Employee() {
+        delete[] name;
     }
+
+    // Move Constructor
+    Employee(Employee&& other) noexcept : id(other.id), age(other.age), name(other.name) {
+        other.name = nullptr; // Transfer ownership and nullify the source
+    }
+
+    // Move Assignment Operator
+    Employee& operator=(Employee&& other) noexcept {
+        if (this != &other) {
+            delete[] name; // Release existing memory
+
+            // Steal resources from other
+            id = other.id;
+            age = other.age;
+            name = other.name;
+
+            other.name = nullptr; // Nullify the source
+        }
+        return *this;
+    }
+
+    // Swap Function using Move Constructor
+    void swap(Employee& other) {
+        Employee temp(std::move(other)); // Move other into temp
+        other = std::move(*this);       // Move this into other
+        *this = std::move(temp);        // Move temp back into this
+    }
+
+  
+    friend ostream& operator<<(ostream& os, const Employee& emp);
 };
+ostream& operator<<(ostream& os, const Employee& emp) {
+    os << emp.id << " " << emp.age << " " << emp.name;
+    return os;
+}
 
 int main() {
     Employee e1(101, 22, "Athira"), e2(102, 23, "Bhagya");
-    cout << e1 << endl;
-    cout << e2 << endl;
-    e1.swap(e2);
-    cout << e1 << endl;
-    cout << e2 << endl;
+
+    cout << e1 << endl; // Output: 101 22 Athira
+    cout << e2 << endl; // Output: 102 23 Bhagya
+
+    e1.swap(e2); // Swap using move constructor
+
+    cout << e1 << endl; // Output: 102 23 Bhagya
+    cout << e2 << endl; // Output: 101 22 Athira
 
     return 0;
-}
