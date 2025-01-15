0f3ff2c22c57ca63cee2d7ef2bf391ca425eec47
Ayisha Dhiyana PS <119615584+AyishaDhiyana@users.noreply.github.com>
2025-01-15 10:30:20 +0530

    Update task03.cpp

--- phase2/evalulation1/task03.cpp
+++ phase2/evalulation1/task03.cpp
@@ -3,74 +3,88 @@
 #include <vector>
 #include <unistd.h>
 #include <sys/types.h>
-#include <climits>
 #include <sys/wait.h>
 
 using namespace std;
 
-class Vaccination
-{
-	private:
-	std::string VaccinationId;
-	int DosesAdministered;
-	
-	public:
-	Vaccination(std::string p_VaccinationId, int p_DosesAdministered) : VaccinationId(p_VaccinationId), DosesAdministered(p_DosesAdministered) {}
-    int getVaccinations() 
-    { 
-        return DosesAdministered;
-    }   
+// Vaccination Class
+class Vaccination {
+private:
+    string VaccinationId;
+    int DosesAdministered;
+
+public:
+    Vaccination(string p_VaccinationId, int p_DosesAdministered)
+        : VaccinationId(p_VaccinationId), DosesAdministered(p_DosesAdministered) {}
+
+    int getVaccinations() { 
+        return DosesAdministered; 
+    }
 };
 
-void server(std::vector<Vaccination>& vaccinations, int& write_to_pipe)
-{
+// Server Function
+void server(vector<Vaccination>& vaccinations, int write_to_pipe) {
     int sum = 0;
-	for(int i = 0; i < vaccinations.size(); i++)
-    {
-        sum = sum + vaccinations[i].getVaccinations();
+    // Calculate the sum of doses
+    for (const auto& vaccination : vaccinations) {
+        sum += vaccination.getVaccinations();
     }
-    write(write_to_pipe,&sum,sizeof(sum));  
-    cout << "server sent sum " << endl;
+    // Write the sum to the pipe
+    write(write_to_pipe, &sum, sizeof(sum));
+    cout << "Server: Sent the sum to the client." << endl;
 }
 
-void client(int read_from_pipe)
-{
+// Client Function
+void client(int read_from_pipe) {
     int sum = 0;
+    // Read the sum from the pipe
     read(read_from_pipe, &sum, sizeof(sum));
-
-   std::cout << "Sum of doses " << sum << std::endl;
-   cout << "client receives sum" << endl;
-    
+    cout << "Client: Received sum of doses = " << sum << endl;
 }
-int main()
-{
-	std::vector<Vaccination> vaccinations;
-    vaccinations.emplace_back("V001", 3);
-    vaccinations.emplace_back("V002", 4);
-    vaccinations.emplace_back("V003", 5);
-    vaccinations.emplace_back("V004", 6);
-    vaccinations.emplace_back("V005", 10);
 
-    int pipe_fd[2];
-    if(pipe(pipe_fd) == -1)
-    {
-        perror("pipe");
+int main() {
+    vector<Vaccination> vaccinations = {
+        {"V001", 3},
+        {"V002", 4},
+        {"V003", 5},
+        {"V004", 6},
+        {"V005", 10}
+    };
+    // Create two pipes for communication
+    int pipe1[2]; // Used by server to send data to client
+    int pipe2[2]; // Used by client to acknowledge receipt (optional)
+
+    if (pipe(pipe1) == -1 || pipe(pipe2) == -1) {
+        perror("Pipe creation failed");
         return 1;
     }
+
     pid_t pid = fork();
-    //child process
-    if(pid == 0)
-    {
-        close(pipe_fd[0]);
-        server(vaccinations,pipe_fd[1]);
-        close(pipe_fd[1]);
+
+    if (pid == -1) {
+        perror("Fork failed");
+        return 1;
     }
-    else
-    {
-        wait(nullptr);
-        close(pipe_fd[1]);
-        client(pipe_fd[0]);
-        close(pipe_fd[0]);
+
+    if (pid == 0) { // Child process: Server
+        close(pipe1[0]); // Close unused read end of pipe1
+        close(pipe2[1]); // Close unused write end of pipe2
+
+        server(vaccinations, pipe1[1]);
+
+        close(pipe1[1]); // Close write end of pipe1 after sending
+        close(pipe2[0]); // Close read end of pipe2 (not used)
+    } else { // Parent process: Client
+        close(pipe1[1]); // Close unused write end of pipe1
+        close(pipe2[0]); // Close unused read end of pipe2
+
+        client(pipe1[0]);
+
+        close(pipe1[0]); // Close read end of pipe1 after reading
+        close(pipe2[1]); // Close write end of pipe2 (not used)
+
+        wait(nullptr); // Wait for the server process to finish
     }
-	return 0;
+
+    return 0;
 }
