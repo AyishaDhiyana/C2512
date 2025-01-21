319e4e7a0410c8bdacc292be7d6d042aa81bd41a
Ayisha Dhiyana PS <119615584+AyishaDhiyana@users.noreply.github.com>
2025-01-21 11:37:44 +0530

    Update main.cpp

--- phase2/case_study/ui/main.cpp
+++ phase2/case_study/ui/main.cpp
@@ -1,9 +1,30 @@
-#include "ui_common.h"
+##include "ui_common.h"
 #include "admin_main.h"
 
-void ManagePatient();
+void ManagePatient() {
+    PatientController controller;
 
-int main() {
-    ManagePatient();
-    return 0;
+    uiCommon.TitleBar("Patient Management System");
+    std::cout << "1. Create Patient\n";
+    std::cout << "2. Edit Patient\n";
+    std::cout << "3. Display Patients\n";
+    std::cout << "4. Exit\n";
+
+    int choice = uiCommon.in.Int("Enter your choice:");
+    switch (choice) {
+        case 1:
+            controller.Create();
+            break;
+        case 2:
+            controller.Edit();
+            break;
+        case 3:
+            controller.Display();
+            break;
+        case 4:
+            std::cout << "Exiting the system. Goodbye!\n";
+            break;
+        default:
+            std::cout << "Invalid choice! Please try again.\n";
+    }
 }
