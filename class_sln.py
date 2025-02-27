4b5164d2274b23184a9f18eea57f199ccca42a93
Ayisha Dhiyana PS <119615584+AyishaDhiyana@users.noreply.github.com>
2025-02-27 14:52:46 +0530

    Update class_sln.py

--- class_sln.py
+++ class_sln.py
@@ -16,38 +16,35 @@
 #Solution
 
 class Employee:
-    str emp_id 
-    str emp_name
-    float emp_salary
-    string emp_department
+    def __init__(emp, emp_id, emp_name, emp_salary, emp_department):
+        emp.emp_id = emp_id
+        emp.emp_name = emp_name
+        emp.emp_salary = emp_salary
+        emp.emp_department = emp_department
 
-    def calculate_emp_salary(self,emp_salary, hours_worked):
+    def calculate_emp_salary(self,hours_worked):
         hours_worked = input("Enter the hours worked by the employee: ")
-
         if (hours_worked > 50):
             overtime = hours_worked - 50
             overtime_amount = (overtime * (emp_salary / 50))
-            print(overtime_amount)
+            total_salary = self.emp_salary + overtime_amount
         else:
-            print(emp_salary)
-
+            total_salary = self.emp_salary
+        return total_salary
+        
     def emp_assign_department(self, emp_department):
         edit = input("Enter the employee id to change the department: ")
         if (edit == emp_department):
             new_department = input("Enter new department: ")
-            emp_department = new_department
+            self.emp_department = new_department
         print(new_department)
     
-    def print_employee_details(self, details):
+    def print_employee_details(self):
         employee_details = input("Enter the employee id to print the details of the employee: ")
         if (employee_details == emp_id):
-
+            print(f"{emp_id}, {emp_name}, {emp_salary}, {emp_department}")
     
-employee1 = Employee("E7876", "ADAMs", 50000, "ACCOUNTING")
+employee1 = Employee("E7876", "ADAMS", 50000, "ACCOUNTING")
 employee2 = Employee("E7499","JONES", 45000, "RESEARCH")
 employee3 = Employee("E7900", "MARTIN", 50000, "SALES")
 employee4 = Employee("E7698", "SMITH", 55000, "OPERATIONS")
-
-
-
-    
