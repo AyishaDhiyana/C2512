df7a38bed7a5b69fe1d42f83af02311bb4db4fe7
Ayisha Dhiyana PS <119615584+AyishaDhiyana@users.noreply.github.com>
2025-02-27 15:21:54 +0530

    Update class_sln.py

--- class_sln.py
+++ class_sln.py
@@ -16,35 +16,83 @@
 #Solution
 
 class Employee:
-    def __init__(emp, emp_id, emp_name, emp_salary, emp_department):
-        emp.emp_id = emp_id
-        emp.emp_name = emp_name
-        emp.emp_salary = emp_salary
-        emp.emp_department = emp_department
+
+    def __init__(employee, emp_id, emp_name, emp_salary, emp_department):
+        employee.emp_id = emp_id
+        employee.emp_name = emp_name
+        employee.emp_salary = emp_salary
+        employee.emp_department = emp_department
 
     def calculate_emp_salary(self,hours_worked):
-        hours_worked = input("Enter the hours worked by the employee: ")
-        if (hours_worked > 50):
+        hours_worked = int(input(f"Enter the hours worked by the employee {self.name}: "))
+
+        if hours_worked > 50:
             overtime = hours_worked - 50
-            overtime_amount = (overtime * (emp_salary / 50))
+            overtime_amount = (overtime * (self.emp_salary / 50))
             total_salary = self.emp_salary + overtime_amount
         else:
             total_salary = self.emp_salary
-        return total_salary
         
-    def emp_assign_department(self, emp_department):
-        edit = input("Enter the employee id to change the department: ")
-        if (edit == emp_department):
+        print(f"Total salary of the employee {self.name} is {total_salary}")
+        
+    def emp_assign_department(self):
+        edit_department = input("Enter the employee id to change the department: ")
+
+        if edit_department == self.emp_department:
             new_department = input("Enter new department: ")
             self.emp_department = new_department
-        print(new_department)
+            print(f"Department updated successfully for {self.emp_name}")
+        else:
+            print("Invalid employee id")
     
     def print_employee_details(self):
-        employee_details = input("Enter the employee id to print the details of the employee: ")
-        if (employee_details == emp_id):
-            print(f"{emp_id}, {emp_name}, {emp_salary}, {emp_department}")
+        employee_details = input("Enter the employee id to view the details of the employee: ")
+
+        if employee_details == self.emp_id:
+            print(f"{self.emp_id}, {self.emp_name}, {self.emp_salary}, {self.emp_department}")
+        else:
+            print("Employee not found")
+
+employees = [   
+Employee("E7876", "ADAMS", 50000, "ACCOUNTING"),
+Employee("E7499","JONES", 45000, "RESEARCH"),
+Employee("E7900", "MARTIN", 50000, "SALES"),
+Employee("E7698", "SMITH", 55000, "OPERATIONS")
+]
+
+while True:
+    print("\n1.Choose an option: ")
+    print("1. Change Department")
+    print("2.Show employee Details")
+    print("3.Calculate overtime salary")
+    print("4. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        for emp in employees:
+            if emp.emp_id == emp_id:
+                emp.emp_assign_department()
+                break
+            else:
+                print("Employee id not found")
     
-employee1 = Employee("E7876", "ADAMS", 50000, "ACCOUNTING")
-employee2 = Employee("E7499","JONES", 45000, "RESEARCH")
-employee3 = Employee("E7900", "MARTIN", 50000, "SALES")
-employee4 = Employee("E7698", "SMITH", 55000, "OPERATIONS")
+    elif choice == "2":
+        emp_id = input("Enter employee id to view the details of the employee: ")
+        for emp in employees:
+            if emp.emp_id == emp_id:
+                emp.print_employee_details()
+                break
+    
+    elif choice == "3":
+        emp_id = input("Enter employee id to calculate the overtime salary: ")
+        for emp in employees:
+            if emp.emp_id == emp_id:
+                emp.calculate_emp_salary()
+                break
+    
+    elif choice == "4":
+        print("Exit")
+
+    else:
+        print("Invalid choice")
